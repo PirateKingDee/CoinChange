d1e3f6f82b9ac1074239afa00412419ac46bc2c2
PirateKingDee <andyliang@b98-aruba-authenticated-10-110-173-200.wlan.cpp.edu>
2017-03-06 12:55:13 -0800

    built backtracking method to solve coin change problem

--- src/coinchange/CoinChange.java
+++ src/coinchange/CoinChange.java
@@ -13,29 +13,69 @@ package coinchange;
 import java.util.LinkedList;
 import java.util.ListIterator;
 public class CoinChange {
-    
-    private LinkedList<Integer> coinSet;
+    LinkedList<Integer> curSet;
+    LinkedList<Integer> changeSet;
+    int min;
     public CoinChange(){
-        coinSet = null;
+        changeSet = new LinkedList();
+        min = Integer.MAX_VALUE;
+        curSet = new LinkedList<>();
     }
-    public CoinChange(LinkedList<Integer> coinSet){
-        this.coinSet = coinSet;
+    public int coinChange(int[] coins, int amount) {
+        return coinChange(0, coins, amount, 0);
     }
-    public LinkedList<Integer> leastCoins(int target){
-        int curAmount = 0;
-        LinkedList<Integer> change = new LinkedList<>();
-        while(curAmount != target){
-            ListIterator coinList = coinSet.listIterator();
-            
+
+    private int coinChange(int idxCoin, int[] coins, int amount, int counter){
+        
+        for(int i = idxCoin; i<coins.length; i++){
+            if(counter < min){
+                if(amount==coins[i]){
+                    counter +=1;
+                    curSet.add(coins[i]);
+                    if(changeSet == null){
+                        changeSet.addAll(curSet);
+                    }
+                    else{
+                        changeSet = new LinkedList(curSet);
+                    }
+                    
+                    min = counter;
+                    curSet.removeLast();
+                    curSet.removeLast();
+
+                    return counter;
+                }
+                else if(amount > coins[i]){
+                    curSet.add(coins[i]);
+                    counter +=1;
+                    coinChange(i, coins, amount-coins[i], counter);
+
+                }
+            } 
+        }
+        if(curSet.isEmpty()){
             
         }
-        return  change;
+        else{
+            curSet.removeLast();
+            
+        }
+        return counter;
+             
+    }  
+    public LinkedList<Integer> getChange(){
+        return changeSet;
     }
+        
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int[] coinSet = {50, 25, 20, 10, 5, 1};
+                CoinChange coin = new CoinChange();
+        System.out.println(coin.coinChange(coinSet, 75));
+        System.out.println(coin.getChange());
     }
     
 }
